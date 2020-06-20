fe4b1278c8994e4e48852e19da852dcfba3de2cf
Rodrigo Casamayor <rodrigo.casamayor@gmail.com>
2020-06-20 17:48:44 +0200

    [REFACTOR] pretty populate_dbms script

--- populate_dbms.sh
+++ populate_dbms.sh
@@ -8,11 +8,19 @@
 # =============================================================================
 
 # PostgreSQL
+echo "PostgreSQL dump..."
+echo "========================================"
 docker container exec -i $(docker-compose ps -q postgres) psql -U posgres tweets <tweets.sql
+echo "\n[ OK ]"
+echo "========================================\n\n"
 
 # MongoDB
+echo "MongoDB dump..."
+echo "========================================"
 docker cp ./tweets.json mongodb:/tmp/
 docker container exec -i $(docker-compose ps -q mongodb) mongoimport --authenticationDatabase admin -u mongodb -p mongodb -d solartweets -c tweets --file /tmp/tweets.json --jsonArray
+echo "\n[ OK ]"
+echo "========================================\n\n"
 
 # Solr
 # docker cp ./tweets.json solr:/opt/solr-8.5.2/example/exampledocs
